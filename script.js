3be1c2a20139289dad0e73fd29333cbbfa2ca463
azurewebstudio-eng <azurewebstudio@gmail.com>
2026-06-11 12:52:46 +0530

    Add files via upload
    Initial commit for CaptionsAI.

---
+++ script.js
@@ -0,0 +1,6 @@
+function generate() {
+  let input = document.getElementById("input").value;
+
+  document.getElementById("output").innerHTML =
+    "🔥 Viral Caption: " + input + " is trending right now! Don't miss it!";
+}
